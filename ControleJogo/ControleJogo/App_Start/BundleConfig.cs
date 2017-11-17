90108a3f447839c424a10d1b890758f9cd54ec30
Angelicogfa <angelicogfa@gmail.com>
2017-11-17 11:30:08 -0200

    Alteraçoes efetuadas

--- ControleJogo/ControleJogo/App_Start/BundleConfig.cs
+++ ControleJogo/ControleJogo/App_Start/BundleConfig.cs
@@ -23,9 +23,9 @@ namespace ControleJogo
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+                      //"~/Content/bootstrap.min.css",
+                      //"~/Content/site.css"));
         }
     }
 }
